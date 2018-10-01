d5b6daff280bcacd28238b4e3a3466c3775b9085
TomAshTee <jaeschke.tomasz@gmail.com>
2018-10-01 20:01:24 +0200

    Initial Commit

---
+++ XYZApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XYZApp
+//
+//  Created by Tomasz Jaeschke on 01/10/2018.
+//  Copyright © 2018 Tomasz Jaeschke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
